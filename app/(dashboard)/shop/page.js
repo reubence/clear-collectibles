7195ed5918d2d26a82683993d7317a8eef42f090
Reuben Rapose <reuben.rapose@gmail.com>
2023-09-07 12:46:42 +0530

    Added Ring Border To SHop

--- app/(dashboard)/shop/page.js
+++ app/(dashboard)/shop/page.js
@@ -165,14 +165,14 @@ function Shop() {
             </div>
             <div className="h-full rounded-2xl flex flex-grow flex-col">
               <ScrollArea className="w-full lg:h-[calc(100vh-458px)] lg:border-b pb-44 lg:pb-4 lg:mb-5">
-                <div className="p-3 grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-6 gap-3">
+                <div className="p-3 grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-6 gap-5">
                   {[...Array(16)].map((_, index) => (
                     <div
                       key={index}
                       className={cn(
-                        "col-span-1 bg-white rounded-2xl max-h-[219px] lg:max-h-[264px] max-w-[200px] p-3 cursor-pointer hover:ring-2 hover:ring-offset-2 hover:ring-primary transition-all",
+                        "col-span-1 bg-white rounded-2xl max-h-[219px] lg:max-h-[264px] max-w-[200px] p-3 cursor-pointer transition-all",
                         {
-                          "ring-2 ring-offset-2 ring-primary":
+                          "ring ring-offset-4 lg:ring-offset-8 ring-ring ring-offset-white/50":
                             selected === index,
                         }
                       )}
