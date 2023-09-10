77d2aed5ac54b8380bf98a5c42687e5f00cc13b7
Reuben Rapose <reuben.rapose@gmail.com>
2023-09-11 02:50:49 +0530

    Edit Avatar

--- components/dashboard-pages/emblems.jsx
+++ components/dashboard-pages/emblems.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { Separator } from "../ui/separator";
 import { ScrollArea } from "../ui/scroll-area";
 
-function Emblems() {
+function Emblems({ emblemData }) {
   return (
     <div className="flex flex-col justify-start gap-4 w-[calc(100vw-80px)] sm:w-[calc(768px-368px)] lg:w-[25vw] lg:h-[54vh]">
       <span className="uppercase text-xl ml-2 col-span-2 text-left whitespace-nowrap">
@@ -13,28 +13,51 @@ function Emblems() {
       <Separator className="col-span-2" />
 
       <ScrollArea className="h-[430px]">
-        <div className="grid grid-cols-2 w-full gap-8">
-          {[...Array(10)].map((_, i) => (
-            <div
-              key={i}
-              className="flex flex-col items-center mx-auto group  hover:border-primary hover:bg-primary/20 hover:cursor-pointer border-2 border-transparent rounded-2xl transition-all duration-500 ease-in-out"
-            >
+        <div className="grid grid-cols-2 w-full h-full gap-8 relative">
+          {emblemData.length > 0 ? (
+            emblemData.map((item, i) => (
+              <div
+                key={i}
+                className="flex flex-col items-center mx-auto group  hover:border-primary hover:bg-primary/20 hover:cursor-pointer border-2 border-transparent rounded-2xl transition-all duration-500 ease-in-out"
+              >
+                <Image
+                  src={item.icon}
+                  alt="Emblem Icon"
+                  height={120}
+                  width={120}
+                  className="rounded-2xl h-[80] w-[80px] lg:h-[120px] lg:w-[120px] aspect-square object-cover"
+                  unoptimized
+                />
+                <h3 className="text-sm font-bold uppercase mt-3">
+                  {item.label}
+                </h3>
+                <p className="text-sm font-normal uppercase text-foreground/50">
+                  {item.subheading}
+                </p>
+              </div>
+            ))
+          ) : (
+            <div className="col-span-2 flex flex-col gap-3 w-full h-full justify-center items-center">
               <Image
                 src={"/images/emblem.svg"}
                 alt="Emblem Icon"
-                height={120}
-                width={120}
-                className="rounded-2xl h-[80] w-[80px] lg:h-[120px] lg:w-[120px] aspect-square object-cover"
+                height={160}
+                width={160}
+                className="rounded-2xl h-[120] w-[120px] lg:h-[160px] lg:w-[160px] aspect-square object-cover mb-3"
                 unoptimized
               />
-              <h3 className="text-sm font-bold uppercase mt-3">
+              <h3 className="text-2xl font-bold uppercase mt-3">
                 SMB Inception
               </h3>
-              <p className="text-sm font-normal uppercase text-foreground/50">
+              <p className="text-xl font-normal uppercase text-foreground/50">
                 subheading
               </p>
+              <p className="text-base text-center font-medium text-foreground/50 mt-7">
+                Attain this prestigious badge by securing a position among the
+                top 100 holders of Bananas
+              </p>
             </div>
-          ))}
+          )}
         </div>
       </ScrollArea>
     </div>
