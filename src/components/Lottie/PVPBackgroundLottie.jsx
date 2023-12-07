81d06ebc8112d522b2b75eff0d97a119c060f58b
Reuben Rapose <reuben.rapose@gmail.com@gmail.com~>
2023-12-07 13:37:38 +0530

    fix(PVPBackgroundLottie): Covers entire screen

--- src/components/Lottie/PVPBackgroundLottie.jsx
+++ src/components/Lottie/PVPBackgroundLottie.jsx
@@ -14,13 +14,27 @@ const PVPBackgroundLottie = () => {
       autoplay: true,
       animationData: animationData,
     });
+    // Set the Lottie animation style to cover the container
+    anim.setSubframe(true);
+    anim.resize();
+    anim.play();
+    anim.addEventListener("DOMLoaded", () => {
+      animationContainer.current.querySelector("svg").style.width = "100%";
+      animationContainer.current.querySelector("svg").style.height = "100%";
+      animationContainer.current.querySelector("svg").style.objectFit = "cover";
+      animationContainer.current.querySelector("svg").style.position =
+        "absolute";
+      animationContainer.current
+        .querySelector("svg")
+        .setAttribute("preserveAspectRatio", "xMidYMid slice");
+    });
 
     return () => anim.destroy(); // optional clean up for unmounting
   });
 
   return (
     <div
-      className="fixed top-0 left-0 w-full h-full overflow-hidden"
+      className="fixed top-0 left-0 w-full h-full z-0 overflow-hidden"
       ref={animationContainer}
     />
   );
