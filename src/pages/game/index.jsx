7ae90032b9779e5dceb2b60319e9036c8036cec7
Reuben Rapose <reuben.rapose@gmail.com@gmail.com~>
2023-12-09 12:59:39 +0530

    feat: Player Seraching Skeletons

--- src/pages/game/index.jsx
+++ src/pages/game/index.jsx
@@ -11,19 +11,16 @@ function Game() {
       {/* LEFT SIDEBAR */}
       <div
         className={cn(
-          "w-full h-full flex flex-col p-8 rounded-lg lg:rounded-2xl border-4 border-gray-700/20 relative",
-          { "flex-col-reverse": !playerFound }
+          "w-full h-full flex flex-col-reverse justify-start rounded-lg lg:rounded-2xl border-4 border-gray-700/20 relative",
+          { "justify-between flex-col": playerFound }
         )}
       >
-        {/* background color when player searching */}
         <div
           className={cn(
-            "absolute w-full bg-gray-800/50 left-0 bottom-0 h-64 rounded-b-lg lg:rounded-b-2xl",
-            { hidden: playerFound }
+            "w-full flex flex-col relative p-8 bg-gray-800/50 left-0 bottom-0 h-64 rounded-b-lg lg:rounded-b-2xl",
+            { "bg-transparent mt-0": playerFound }
           )}
-        />
-
-        <div className={cn("flex flex-col relative")}>
+        >
           <div>
             <Image
               src="/background/mythic-badge.svg"
@@ -42,78 +39,95 @@ function Game() {
             <Separator className="h-6 rounded-full w-0.5 bg-white/80" />
             <span className="text-3xl font-normal">LV.2</span>
           </div>
+          <Separator
+            className={cn(
+              "h-0.5 rounded-full w-full bg-gray-700/20 absolute left-0 bottom-6",
+              {
+                hidden: !playerFound,
+              }
+            )}
+          />
         </div>
-
-        <Separator
-          className={cn("h-[1px] rounded-full w-full bg-white/60 my-8", {
-            hidden: !playerFound,
-          })}
-        />
 
         {/* nft-image */}
         <Image
           src="/images/nft-pvp.png"
+          alt="Dashboard Nft Image"
+          width={1000}
+          height={1000}
+          // mirrored
+          className={cn("", { "": !playerFound })}
+        />
+      </div>
+
+      {/* GAME SQUARE */}
+      <div className="w-full h-full p-5 rounded-lg lg:rounded-2xl border-4 border-gray-700/20 aspect-square"></div>
+
+      {/* RIGHT SIDEBAR */}
+      <div
+        className={cn(
+          "w-full h-full flex flex-col-reverse justify-start rounded-lg lg:rounded-2xl border-4 border-gray-700/20 relative",
+          { "justify-between flex-col": playerFound }
+        )}
+      >
+        <div
+          className={cn(
+            "w-full flex flex-col relative p-8 bg-gray-800/50 left-0 bottom-0 h-64 rounded-b-lg lg:rounded-b-2xl",
+            {
+              "bg-transparent mt-0": playerFound,
+              "animate-pulse justify-center": !playerFound,
+            }
+          )}
+        >
+          {/* PLAYER SEARCHING */}
+          <div className="text-white font-semibold text-[28px] text-center">
+            Searching...
+          </div>
+          <div className={cn({ hidden: !playerFound })}>
+            <Image
+              src="/background/mythic-badge.svg"
+              alt="Dashboard Nft Image"
+              width={220}
+              height={70}
+              className="z-10"
+            />
+            <span className="absolute translate-x-20 top-8 text-[#B28AF8]">
+              Mythic
+            </span>
+          </div>
+
+          <div
+            className={cn(
+              "flex gap-4 my-4 font-g8 font-bold text-6xl text-white items-baseline z-40",
+              { hidden: !playerFound }
+            )}
+          >
+            #852
+            <Separator className="h-6 rounded-full w-0.5 bg-white/80" />
+            <span className="text-3xl font-normal">LV.2</span>
+          </div>
+          <Separator
+            className={cn(
+              "h-0.5 rounded-full w-full bg-gray-700/20 absolute left-0 bottom-6",
+              {
+                hidden: !playerFound,
+              }
+            )}
+          />
+        </div>
+
+        {/* nft-image */}
+        <Image
+          src={
+            playerFound ? "/images/nft-pvp.png" : "/images/nft-searching.png"
+          }
           alt="Dashboard Nft Image"
           width={1000}
           height={1000}
           // mirrored
           className={cn("", {
-            "!block mb-[60px]": !playerFound,
-            "absolute bottom-0 left-0": playerFound,
-          })}
-        />
-      </div>
-      <div className="w-full h-full p-5 rounded-lg lg:rounded-2xl border-4 border-gray-700/20 aspect-square"></div>
-      {/* RIGHT SIDEBAR */}
-      <div
-        className={cn(
-          "w-full h-full flex flex-col items-end p-8 rounded-lg lg:rounded-2xl border-4 border-gray-700/20 relative",
-          { "flex-col-reverse": !playerFound }
-        )}
-      >
-        {/* background color when player searching */}
-        <div
-          className={cn(
-            "absolute w-full bg-gray-800/50 left-0 bottom-0 h-64 rounded-b-lg lg:rounded-b-2xl",
-            { hidden: playerFound }
-          )}
-        />
-
-        <div className="relative">
-          <Image
-            src="/background/mythic-badge.svg"
-            alt="Dashboard Nft Image"
-            width={220}
-            height={70}
-            className="z-10"
-          />
-          <span className="absolute translate-x-20 top-8 text-[#B28AF8]">
-            Mythic
-          </span>
-        </div>
-
-        <div className="flex gap-4 my-4 font-g8 font-bold text-6xl text-white items-baseline z-40">
-          #852
-          <Separator className="h-6 rounded-full w-0.5 bg-white/80" />
-          <span className="text-3xl font-normal">LV.2</span>
-        </div>
-
-        <Separator
-          className={cn("h-[1px] rounded-full w-full bg-white/60 my-8", {
-            hidden: !playerFound,
-          })}
-        />
-
-        {/* nft-image */}
-        <Image
-          src="/images/nft-pvp.png"
-          alt="Dashboard Nft Image"
-          width={1000}
-          height={1000}
-          // mirrored
-          className={cn("-scale-x-100 a", {
-            "!block mb-[60px]": !playerFound,
-            "absolute bottom-0 right-0": playerFound,
+            "-scale-x-100": playerFound,
+            "animate-pulse": !playerFound,
           })}
         />
       </div>
