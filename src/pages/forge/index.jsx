37d57e85739bc382f08a93e9310fdbd574d97445
Reuben Rapose <reuben.rapose@gmail.com@gmail.com~>
2024-01-03 14:44:35 +0530

    Refactor game mode buttons and play button placement

--- src/pages/forge/index.jsx
+++ src/pages/forge/index.jsx
@@ -93,7 +93,6 @@ let tabs = [
 
 export default function Forge() {
   const [activeNFT, setActiveNFT] = useState(selectData[2]);
-  const [gameMode, setGameMode] = useState("PVP");
   const [rarity, setRarity] = useState("Mythic");
   const [rewardPointsNeeded, setRewardPointsNeeded] = useState(300);
   const [rewardProgress, setRewardProgress] = useState(346);
@@ -124,7 +123,7 @@ export default function Forge() {
       <PVPBackgroundLottie />
 
       {/* Forge IMAGE  */}
-      <div className="absolute left-1/2 -translate-x-1/2 bottom-1/2 translate-y-1/2">
+      <div className="absolute left-[40vw] top-[20vh] xl:left-1/2 xl:-translate-x-1/2">
         <div className="">
           {/* <Image
             unoptimised
@@ -193,126 +192,38 @@ export default function Forge() {
       {/* GAME MODE BUTTONS */}
       <div className="absolute flex gap-3 right-8 bottom-8 xl:right-12 xl:bottom-12 ">
         <div className="absolute flex flex-col gap-1.5 xl:gap-2.5 items-end bottom-20 md:bottom-28 lg:bottom-36 xl:bottom-60 right-0">
-          <div className="flex gap-4 items-baseline font-bold text-3xl xl:text-6xl text-white">
-            <span className="text-base xl:text-3xl font-normal">
-              LV.{activeNFT.level}
-            </span>
-            <Separator className="h-4 xl:h-6 rounded-full w-0.5 bg-white/80" />#
-            {activeNFT.id}
-          </div>
           <div className="bg-black/30 rounded-lg xl:rounded-2xl py-2.5 px-4 font-bold text-xs xl:text-2xl text-white whitespace-nowrap">
             Total Bubbles 🫧: {activeNFT.bubbles.toLocaleString()}
           </div>
         </div>
 
-        {/* MOBILE PLAY BUTTON WITH POPUP */}
-        <Dialog>
-          <DialogTrigger
-            className={cn(
-              "xl:hidden py-3",
-              buttonVariants({ variant: "game" })
-            )}
-          >
-            {" "}
-            <Icons.controller className="w-7 h-7 fill-[#353533]" />
-            PLAY!
-          </DialogTrigger>
-          <DialogContent className="h-fit w-fit p-5 bg-primary border-none !rounded-xl">
-            <DialogHeader className="">
-              <DialogTitle className="text-white text-sm font-bold text-center">
-                Choose a game type{" "}
-              </DialogTitle>
-            </DialogHeader>
-            <Tabs className="-mt-1">
-              <TabsList className="grid w-full grid-cols-2 h-full gap-2 rounded-[10px] p-2 text-white bg-black/30">
-                <TabsTrigger
-                  value="head"
-                  className="gap-2 px-6 py-1 xl:px-10 xl:py-3 rounded-md data-[state=active]:text-primary data-[state=active]:bg-white"
-                  onClick={() => setGameMode("PVP")}
-                  data-state={gameMode === "PVP" ? "active" : ""}
-                >
-                  <Icons.controller className="w-7 h-7 rotate-12 fill-primary" />
-                  PVP
-                </TabsTrigger>
-                <TabsTrigger
-                  value="tail"
-                  className="gap-2 px-6 py-1 xl:px-10 xl:py-3 rounded-md data-[state=active]:text-primary data-[state=active]:bg-white"
-                  onClick={() => setGameMode("Coin Flip")}
-                  data-state={gameMode === "Coin Flip" ? "active" : ""}
-                >
-                  <Icons.coin className="w-full h-full mr-2.5" />
-                  Coin Flip
-                </TabsTrigger>
-              </TabsList>
-            </Tabs>
-
-            <p className="text-center mt-3 whitespace-nowrap text-base xl:text-2xl font-bold text-white">
-              {" "}
-              Play Need :{" "}
-              <span className="px-2.5 py-1.5 bg-black/30 rounded-lg ">
-                🫧243
-              </span>
-            </p>
-
-            <Button variant="game" className="w-fit mx-auto">
-              Play
-            </Button>
-          </DialogContent>
-        </Dialog>
-
         {/* DESKTOP PLAY BUTTON */}
-        <div className="hidden xl:flex relative">
+        <div className="flex w-48 xl:w-96 relative">
           {/* bubbles required green */}
-          <div className="hidden w-[calc(100%-2px)] xl:block whitespace-nowrap absolute -top-11 text-xl font-bold text-white bg-green-500 pt-2 pb-6 px-3 rounded-t-2xl">
+          <div className="hidden w-full xl:block whitespace-nowrap absolute -top-11 text-xl font-bold text-white bg-green-500 pt-2 pb-6 px-3 rounded-t-2xl">
             Bubbles required:{" "}
             <span className="px-2.5 py-1.5 bg-black/30 rounded-lg">🫧234</span>
           </div>
 
-          <Button
-            variant="game"
-            className={cn(
-              "text-base xl:text-[40px] font-semibold font-g8 text-[#353533] !rounded-r-none z-40"
-            )}
-          >
-            <Icons.controller className="w-16 h-16 fill-[#353533]" />
-            {gameMode}
-          </Button>
-          <Select className="relative">
-            <SelectTrigger
+          <div className="flex w-full">
+            <Button
+              variant="game"
               className={cn(
-                "!rounded-l-none",
-                buttonVariants({ variant: "gameSecondary" })
+                "!w-full !justify-center bg-yellow-400 hover:bg-yellow-500 border-yellow-600 !py-2.5 xl:!py-10",
+                "text-base xl:text-[40px] !font-bold font-g8 text-[#353533] justify-between z-40 w-full !rounded-r-none"
               )}
-            ></SelectTrigger>
-            <SelectContent
-              className={cn({
-                "data-[side=top]:!w-[428px]": gameMode === "Coin Flip",
-              })}
             >
-              <Tabs className="">
-                <TabsList className="grid w-full grid-cols-2 h-full gap-2 rounded-[10px] p-2 text-white bg-[#0085CF]">
-                  <TabsTrigger
-                    value="pvp"
-                    className="gap-2 p-1 rounded-[10px] data-[state=active]:text-primary data-[state=active]:bg-white"
-                    onClick={() => setGameMode("PVP")}
-                    data-state={gameMode === "PVP" ? "active" : ""}
-                  >
-                    <Icons.controller className="w-7 h-7 rotate-12 fill-primary" />
-                    PVP
-                  </TabsTrigger>
-                  <TabsTrigger
-                    value="coin-flip"
-                    className="gap-2 p-1 px-2 rounded-[10px] data-[state=active]:text-primary data-[state=active]:bg-white"
-                    onClick={() => setGameMode("Coin Flip")}
-                    data-state={gameMode === "Coin Flip" ? "active" : ""}
-                  >
-                    <Icons.coin className="w-full h-full mr-2.5" />
-                    Coin Flip
-                  </TabsTrigger>
-                </TabsList>
-              </Tabs>
-            </SelectContent>
-          </Select>
+              Forge{" "}
+            </Button>
+            <Button
+              variant="game"
+              className={cn(
+                "flex w-fit  items-center justify-center !rounded-l-none !py-1.5 xl:!py-5"
+              )}
+            >
+              <Icons.controller className="w-8 h-8 xl:w-16 xl:h-16 fill-[#353533]" />
+            </Button>
+          </div>
         </div>
       </div>
     </main>
